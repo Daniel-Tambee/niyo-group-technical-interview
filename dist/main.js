01a7b21edb207f6b47122c197531392d49f8b3b7
Daniel-Tambee <danieltambee@gmail.com>
2024-05-18 11:04:45 +0100

    even more changes

--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
+const common_1 = require("@nestjs/common");
+const bodyParser = require("body-parser");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    app.enableCors({
+        origin: ['http://localhost:3000', '*'],
+    });
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('Niyo Group Technical Interview')
+        .setContact('daniel tambee', '', 'danieltambee@gmail.com')
+        .setVersion('1.0')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+    await app.listen(process.env.WORKER_PORT || 3000);
+    const logger = new common_1.Logger('App', {
+        timestamp: true,
+    });
+    logger.log('app running on ' + 3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: ['http://localhost:3000', '*'],
+  });
+  const config = new DocumentBuilder()
+    .setTitle('Niyo Group Technical Interview')
+    .setContact('daniel tambee', '', 'danieltambee@gmail.com')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
+  await app.listen(process.env.WORKER_PORT || 3000);
+  const logger: Logger = new Logger('App', {
+    timestamp: true,
+  });
+  logger.log('app running on ' + 3000);
 }
 bootstrap();
